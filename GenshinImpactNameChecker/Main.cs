838c63063bbe3a37f18666860d7dd7520a1c51ab
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2021-10-03 16:32:47 +0900

    Version1.0.0 Release
    Release

---
+++ GenshinImpactNameChecker/Main.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenshinImpactNameChecker
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Albedo");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Lisa");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Mona");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Ningguang");
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Noelle");
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Qiqi");
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+         }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Aloy");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Amber");
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Barbara");
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Beidou");
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Bennett");
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Chongyun");
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Diluc");
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Diona");
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Eula");
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Fischl");
+        }
+
+        private void button12_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Ganyu");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Hu Tao");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Jean");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Kazuha");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Kaeya");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Ayaka");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Keqing");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Klee");
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Kujou Sara");
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Raiden");
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Razor");
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Rosaria");
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Sayu");
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Sucrose");
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Tartaglia");
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Venti");
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Xiangling");
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Xiao");
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Xingqiu");
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Xinyan");
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Yanfei");
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Yoimiya");
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Zhongli");
+        }
+    }
+}
